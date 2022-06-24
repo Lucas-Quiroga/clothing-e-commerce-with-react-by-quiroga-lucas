b1123716b8a77214bbc9ad49484ed2717561ad15
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2022-06-23 22:28:17 -0300

    challenge finished

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import './App.css';
 // COMPONENTES
 import Header from "./Components/Header/Header"
 import ItemListContainer from './Components/ItemListContainer';
+import ItemCount from "./Components/ItemCount"
+import { useState } from 'react';
 
 const App = () => {
+
+  const onadd=(value)=>{
+    alert(`Adding ${value} products`)
+  }
+
   return (
     <div className='App'>
     <Header />
     <ItemListContainer greeting="Welcome to my clothing website"/>
+    <ItemCount intial={1} stock={10} onadd={onadd}/>
     </div>
   );
 }
